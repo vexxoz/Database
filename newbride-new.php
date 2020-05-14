cd50ccd4d4c93a389dc3af8053cbf7599983c791
Blake Caldwell <blakecaldwell123@gmail.com>
2020-05-14 12:21:56 -0700

    Added dynamic dates
    - Added the dynamic creation of dates for dropdown to keep menu's always up to date
    - Fixed squishing issues with buttons and dropdowns

--- newbride-new.php
+++ newbride-new.php
@@ -44,25 +44,20 @@ if(isset($_SESSION['cid']) && $_SESSION['cid'] > 0 && $_SESSION['isAdmin'] >= 1)
 		<button class="col-md-1 col-sm-2 col-xs-2 col-lg-1 menubar offset-md-1 offset-lg-1" onclick="edit(0)">New Bride</button>
 	  <input class="col-md-5 col-sm-8 col-xs-8 col-lg-5" type="text" id="search" name="search" placeholder="Type to start search" autocomplete="off">
 	  <select class="col-md-1 col-sm-2 col-xs-2 col-lg-1 menubar" type="text" id="year" name="year" required="">
-	    <option value="ALL">ALL</option>
-			<option value="2025">2025</option>
-	    <option value="2024">2024</option>
-	    <option value="2023">2023</option>
-	    <option value="2022">2022</option>
-	    <option value="2021">2021</option>
-	    <option value="2020" selected>2020</option>
-	    <option value="2019">2019</option>
-	    <option value="2018">2018</option>
-	    <option value="2017">2017</option>
-	    <option value="2016">2016</option>
-	    <option value="2015">2015</option>
-	    <option value="2014">2014</option>
-	    <option value="2013">2013</option>
-	    <option value="2012">2012</option>
-	    <option value="2011">2011</option>
-	    <option value="2010">2010</option>
-	    <option value="2009">2009</option>
-	    <option value="2008">2008</option>
+			<option value="ALL">ALL</option>
+			<?php
+				$currentYear = (int)date("Y");
+				$endYear = 2009;
+				// dynamically keep the dates current
+				for($i=$currentYear+4;$i>=$endYear;$i--){
+					if($i == $currentYear){
+						echo "<option value='".$i."' selected>".$i."</option>";
+					}else{
+						echo "<option value='".$i."'>".$i."</option>";
+					}
+				}
+
+			 ?>
 	  </select>
 		<div class="dropdown col-md-1 col-sm-2 col-xs-2 col-lg-1">
 		  <button class="btn btn-secondary dropdown-toggle" type="button" id="cbdButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -70,10 +65,8 @@ if(isset($_SESSION['cid']) && $_SESSION['cid'] > 0 && $_SESSION['isAdmin'] >= 1)
 		  </button>
 		  <div class="dropdown-menu" aria-labelledby="cbdButton">
 				<?php
-				$endYear = (int)date("Y");
-				$startYear = 2009;
-
-				for($i=$endYear;$i>=$startYear;$i--){
+				// dynamically keep the dates current
+				for($i=$currentYear;$i>=$endYear;$i--){
 					echo "<a class='dropdown-item' href='opencbd.php?year=".$i."'>".$i."</a>";
 				}
 
